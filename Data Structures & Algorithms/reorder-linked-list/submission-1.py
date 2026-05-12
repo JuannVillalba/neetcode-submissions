5e0c13882465e5e3217fa29f5a4d6c0f794a5a4a
Juan Pablo Villalba <juanpablovillalba02@gmail.com>
2026-05-11 20:17:54 -0700

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        slow = head
+        fast = head.next
+        while fast and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = None
+        slow.next = None
+
+        while second is not None:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        first , second = head , prev
+        while second is not None:
+            temp1 , temp2 = first.next , second.next
+            first.next = second
+            second.next = temp1
+            first , second = temp1 , temp2
+        
+
+
+        
+
